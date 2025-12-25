269bfeba586a9c7215060dbf767e0363cf996113
David <davidliebtkekse@gmail.com>
2025-12-25 18:48:11 +0100

    feat: focus update and timer update
    feat: only refresh on timer if document is visible as that will cause a refresh eventually anyway

--- src/lib/auth/AuthWrapper.tsx
+++ src/lib/auth/AuthWrapper.tsx
@@ -2,6 +2,7 @@
 
 import {ReactNode, useEffect} from "react";
 import {useSessionStore} from "@/lib/auth/store";
+import {clearInterval} from "node:timers";
 
 
 export function AuthWrapper({ children }: Readonly<{ children: ReactNode }>) {
@@ -9,10 +10,27 @@ export function AuthWrapper({ children }: Readonly<{ children: ReactNode }>) {
     const updateSession = useSessionStore((it) => it.updateSession)
 
     useEffect(() => {
-        updateSession()
-            .catch(err => {
-                console.error(err);
-            });
+        function updateSessionInBackground() {
+            updateSession()
+                .catch(err => {
+                    console.error("Failed to update session", err);
+                });
+        }
+        updateSessionInBackground()
+
+        const visibilityHandler = () => { if (document.visibilityState === "visible") updateSessionInBackground(); }
+        document.addEventListener("visibilitychange", visibilityHandler, false)
+
+        const updateTimer = setInterval(() => {
+            if (document.visibilityState === "visible") { //Dont refresh forever in the background
+                updateSessionInBackground();
+            }
+        }, 10 * 60 * 1000); // every 10 minutes
+
+        return () => {
+            document.removeEventListener("visibilitychange", visibilityHandler, false)
+            clearInterval(updateTimer)
+        }
     }, [updateSession])
 
     return children;
