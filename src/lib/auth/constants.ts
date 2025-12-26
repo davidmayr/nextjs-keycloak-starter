3b86aa66a00f35bf17b9774be8467a5ce0a6e836
David <davidliebtkekse@gmail.com>
2025-12-26 11:27:56 +0100

    fix: missing imports

--- src/lib/auth/constants.ts
+++ src/lib/auth/constants.ts
@@ -1,3 +1,6 @@
+import {RequestCookies, ResponseCookies} from "next/dist/compiled/@edge-runtime/cookies";
+import {ReadonlyRequestCookies} from "next/dist/server/web/spec-extension/adapters/request-cookies";
+
 export const COOKIE_PREFIX = "ses_"
 
 export type Cookies = RequestCookies | ResponseCookies | ReadonlyRequestCookies
